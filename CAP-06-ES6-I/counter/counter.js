656083f48a7df83f9fd8889a44a58de2881dcf96
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2023-12-21 10:31:27 +0545

    Added Counter Class

--- CAP-06-ES6-I/counter/counter.js
+++ CAP-06-ES6-I/counter/counter.js
@@ -1,49 +1,73 @@
- 
+
 let root = document.querySelector('h1');
 document.querySelector('#start').addEventListener('click',start);
 document.querySelector('#add').addEventListener('click',increament)
 document.querySelector('#sub').addEventListener('click',decreament)
 
 
-let start_stop = 0;
+ 
 let clear;
-var count = 1;   
+let count = 0;
+class Counter{
+    constructor(){ 
+
+    } 
+    start(){
+    clear =  setInterval(() => {
+          count++;
+          root.innerHTML = count;
+        }, 1000);
+        
+      }
+      stop(){
+        clearInterval(clear);
+      } 
+      increase(){ 
+      clearInterval(clear);
+      count++;
+      root.innerHTML = count;
+      }
+      decrease(){ 
+      clearInterval(clear)
+      count--;
+      root.innerHTML = count;
+
+     }
+      
+
+}   
+
+let counter = new Counter();  
+let stspcount = 0;
 function start(){
-  start_stop++;
-  if(start_stop % 2 == 1){    
-    document.querySelector('#start').textContent = 'Stop';
-    clear = setInterval(() => { 
-      root.innerHTML = count++;
-    }, 1000); 
+   stspcount++; 
+  if(stspcount % 2 == 1){
+  counter.start();
+  document.querySelector('#start').textContent = 'Stop';
   }else{
-    document.querySelector('#start').textContent = 'Start';
-    clearInterval(clear);
-    count = count; 
-  }  
-}
- 
-
-
-let increament_count = 0;
-
-function increament(st){ 
-  increament_count++;  
-  clearInterval(clear);
-  if(increament_count > 1){  
-    count = +root.innerHTML + 1;
-    root.innerHTML = +root.innerHTML + 1;
-    document.querySelector('#start').textContent = 'Start';
+    counter.stop();
+    document.querySelector('#start').textContent = 'Start';      
   }
- 
 }
 
+
+incdec_counter = 0;
+function increament(){ 
+  incdec_counter++;
+  if(incdec_counter>1){
+    counter.increase();
+  }else{
+    counter.stop();
+  } 
+    
+  }
+  
 function decreament(){
-  increament_count++;
-  if(increament_count > 1){ 
-    clearInterval(clear);
-    let plus = root.innerHTML;
-    root.innerHTML = +plus - 1;
+  incdec_counter++;
+  if(incdec_counter > 1){
+    counter.decrease(); 
+  }else{
+    counter.stop();
   }
 }
 
- 
